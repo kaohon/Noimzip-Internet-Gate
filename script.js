6a77a11349a64ff0bfbed0c16afbd4890b5fae51
N o i m z i p <74817397+kaohon@users.noreply.github.com>
2024-07-19 00:07:15 +0900

    Add search

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-var home_c = document.getElementById('home-contents');
-var eaten_c = document.getElementById('eaten-contents');
-var ezos_c = document.getElementById('ezos-contents');
+var search_c = document.getElementById('search-contents');
+var memo_c = document.getElementById('memo-contents');
+var audio_c = document.getElementById('audio-contents');
 var profile_c = document.getElementById('profile-contents');
 
-var home_t = document.getElementById('home-tab');
-var eaten_t = document.getElementById('eaten-tab');
-var ezos_t = document.getElementById('ezos-tab');
+var search_t = document.getElementById('search-tab');
+var memo_t = document.getElementById('memo-tab');
+var audio_t = document.getElementById('audio-tab');
 var profile_t = document.getElementById('profile-tab');
 
-home_t.addEventListener("click", function () {
-    home_c.style.display = "block";
-    eaten_c.style.display = "none";
-    ezos_c.style.display = "none";
+search_t.addEventListener("click", function () {
+    search_c.style.display = "block";
+    memo_c.style.display = "none";
+    audio_c.style.display = "none";
     profile_c.style.display = "none";
   });
 
-eaten_t.addEventListener("click", function () {
-    home_c.style.display = "none";
-    eaten_c.style.display = "block";
-    ezos_c.style.display = "none";
+memo_t.addEventListener("click", function () {
+    search_c.style.display = "none";
+    memo_c.style.display = "block";
+    audio_c.style.display = "none";
     profile_c.style.display = "none";
   });
 
-ezos_t.addEventListener("click", function () {
-    home_c.style.display = "none";
-    eaten_c.style.display = "none";
-    ezos_c.style.display = "block";
+audio_t.addEventListener("click", function () {
+    search_c.style.display = "none";
+    memo_c.style.display = "none";
+    audio_c.style.display = "block";
     profile_c.style.display = "none";
   });
 
 profile_t.addEventListener("click", function () {
-    home_c.style.display = "none";
-    eaten_c.style.display = "none";
-    ezos_c.style.display = "none";
+    search_c.style.display = "none";
+    memo_c.style.display = "none";
+    audio_c.style.display = "none";
     profile_c.style.display = "block";
   });
 
